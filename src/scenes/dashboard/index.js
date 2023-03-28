e24662c5a325cd635d6620f245471bad5aa06c70
baji <baji8143@gmail.com>
2023-03-28 13:47:55 +0530

    initial

---
+++ src/scenes/dashboard/index.js
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material"
+import Header from "../../components/Header"
+
+
+const index = () => {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Header title="DASHBOARD" subtitle="welcome to your dashboard"/>
+      </Box>
+    </Box>
+  )
+}
+
+export default index
